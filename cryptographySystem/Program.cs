2c2c028650ed17906e22907fbee6a31d58780f16
Gina Guidotti <gina.guidotti@jacks.sdstate.edu>
2019-12-01 21:07:46 -0600

    Finalized Changes for Program.cs and VigenereCipher.cs and created new class CaesarCipher.cs

--- cryptographySystem/Program.cs
+++ cryptographySystem/Program.cs
@@ -13,36 +13,77 @@ namespace cryptographySystem
     {
         public static void Main(string[] args)
         {
-            const string key = "softwarearchitecture";
-            string plaintext = "";
-            string encrypttext = "";
-            string decrypttext = ""; 
-            VigenereCipher theCipher = new VigenereCipher();
-            Console.WriteLine("Enter a string to be encrypted: ");
-            plaintext = Console.ReadLine();
+            string key = "softwarearchitecture";    //Default key value            
+            int choice = 1;                         //Default to Vigenere Cipher First
+            string plaintext, encrypttext, decrypttext = "";
 
-            /* Pipe 1 */
-            Console.Write("Plaintext ");
-            theCipher.PrintStringText(plaintext);
 
-            /* Filter 1 */
-            encrypttext = theCipher.Encrypt(plaintext, key);
+            Console.WriteLine("Welcome to the Cryptography System");
+            Console.WriteLine("\nThe purpose of this system is to show how the Vigenere and Caesar cipher works.");
+            
+            do
+            {
+                Console.WriteLine("==================");
+                Console.WriteLine("Menu Options: ");
+                Console.WriteLine("==================");
+                Console.WriteLine("1. Vigenere Cipher");
+                Console.WriteLine("2. Caesar Cipher ");
+                Console.WriteLine("0. Exit System ");
+                Console.WriteLine("==================");
+                Console.WriteLine("\nEnter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
 
-            /* Pipe 2 */
-            Console.Write("Encrypted ");
-            theCipher.PrintStringText(encrypttext);
+                switch (choice) 
+                {
+                    case 1: 
+                        //Vigenere Cipher
+                        VigenereCipher theVCipher = new VigenereCipher();
+                        Console.WriteLine("\nEnter a string to be encrypted: ");
+                        plaintext = Console.ReadLine();
 
-            /* Filter 2 */
-            decrypttext = theCipher.Decrypt(encrypttext, key);
+                        Console.WriteLine("Enter a string to be used as the key: ");
+                        key = Console.ReadLine();
 
-            /* Pipe 3 */
-            Console.Write("Decrypted ");
-            theCipher.PrintStringText(decrypttext);
+
+                        Console.Write("Plaintext Given: ");
+                        theVCipher.PrintStringText(plaintext);
+
+                        encrypttext = theVCipher.Encrypt(plaintext, key);
+
+                        Console.Write("Encrypted String:  ");
+                        theVCipher.PrintStringText(encrypttext);
+
+                        decrypttext = theVCipher.Decrypt(encrypttext, key);
+
+                        Console.Write("Decrypted String:  ");
+                        theVCipher.PrintStringText(decrypttext);
+                        break;
+
+                    case 2:
+                        //Caesar Cipher
+                        CaesarCipher theCCipher = new CaesarCipher();
+                        Console.WriteLine("\nEnter a string to be encrypted: ");
+                        plaintext = Console.ReadLine();
+
+                        Console.WriteLine("Enter an integer to be used as the key: ");
+                        int caesarKey = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Encrypted Data: ");
+                        encrypttext = theCCipher.Encipher(plaintext, caesarKey);
+                        Console.WriteLine(encrypttext);
+
+                        Console.WriteLine("Decrypted Data: ");
+                        decrypttext = theCCipher.Decipher(encrypttext, caesarKey);
+                        Console.WriteLine(decrypttext);
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Thank you for using the Cryptography System."); 
+                        break;
+                }                 
+            } while (choice != 0);
         }
 
-
-    }
-
-
+    }   
 
 }
